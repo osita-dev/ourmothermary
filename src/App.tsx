218a567c85bab5213d70ba189088b82cbd4d8a6d
osita kizito <ositakizito4@gmail.com>
2026-08-14 05:48:37 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Devotions from "./pages/Devotions";
 import Notifications from "./pages/Notifications";
 import CalendarPage from "./pages/CalendarPage";
 import More from "./pages/More";
+import DayReadingPage from "./pages/DayReadingPage";
 import GamePage from "./pages/GamePage";
 import NotFound from "./pages/NotFound";
 
@@ -29,7 +30,8 @@ const App = () => (
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/session/:sessionId" element={<SessionPlayer />} />
-               <Route path="/game" element={<GamePage />} />
+                  <Route path="/game" element={<GamePage />} />
+                <Route path="/session/:sessionId/day/:dayNumber" element={<DayReadingPage />} />
                 <Route path="/devotions" element={<Devotions />} />
                 <Route path="/notifications" element={<Notifications />} />
                 <Route path="/calendar" element={<CalendarPage />} />
